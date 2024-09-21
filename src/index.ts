478536938681e2518f03ae9f37db558940ed32f1
Arjen Postma <info@postma-dev.nl>
2024-09-21 14:26:02 +0200

    Updated build process

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { container } from './Container';
+export { Container, container } from './Container';
 export { Inject, Injectable } from './Decorators';
+
+// Export types
 export type { Container as IContainer } from './Container';
+export type { InjectDecorator, InjectableDecorator } from './Decorators';
